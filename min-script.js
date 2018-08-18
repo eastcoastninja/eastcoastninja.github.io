af428738153a371cf2a4d876cab2e0fb088f3d85
Julian Rios <julianrios.dev@gmail.com>
2018-08-17 22:57:31 -0400

    added mini-js & mini-css

---
+++ min-script.js
@@ -0,0 +1,1 @@
+let options={strings:["Hi, my name is Julian."],typeSpeed:75},typed=new Typed(".display-4",options);document.querySelectorAll(".typed-cursor")[0].style.display="none";
--- script.js
+++ script.js
@@ -2,7 +2,5 @@ let options = {
   strings: ["Hi, my name is Julian."],
   typeSpeed: 75
 }
-
 let typed = new Typed(".display-4", options);
-
 document.querySelectorAll(".typed-cursor")[0].style.display = "none"; 
